619919170f62cf9b5b0982bb9c1876dd762d8bbb
yizhiyu <yizhiyu_web@163.com>
2020-05-13 10:06:37 +0800

    改进计数排序

--- 排序算法/9.计数排序.js
+++ 排序算法/9.计数排序.js
@@ -6,6 +6,15 @@ count sort (计数排序)
 ——————————————————————————————————————————
 
 改进：
+改进后实现可保存正负数；稳定性变为稳定
+k
+----------------------------------------------------------------------------
+index
+----------------------------------------------------------------------------
+count
+
+index = k - min
+count = arr[index -1] + n
 */
 
 const arr = [23, 11, 54, 2, 3, 11, 9, 9, 9, 6, 65, 78, 13, 9, 32, 69, 90, 20, 45, 76, 20];
@@ -35,4 +44,36 @@ function countSort(arr, maxValue) {
   return arr;
 }
 
-console.log(countSort(arr, Math.max(...arr)));
+function countSortPro(arr, maxValue, minValue) {
+  // 开辟空间
+  const countArr = new Array(maxValue - minValue + 1);
+
+  // 计数
+  for (let i = 0; i < arr.length; i++) {
+    if (!countArr[arr[i] - minValue]) {
+      countArr[arr[i] - minValue] = 0;
+    }
+    countArr[arr[i] - minValue]++;
+  }
+
+  for (let i = 1; i < countArr.length; i++) {
+    if (!countArr[i - 1]) {
+      countArr[i - 1] = 0;
+    }
+    if (!countArr[i]) {
+      countArr[i] = 0;
+    }
+    countArr[i] += countArr[i - 1];
+  }
+
+  // 放入相应位置
+  const result = new Array(arr.length);
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result[countArr[arr[i] - minValue] - 1] = arr[i];
+    countArr[arr[i] - minValue]--;
+  }
+
+  return result;
+}
+
+console.log(countSortPro(arr, Math.max(...arr), Math.min(...arr)));
