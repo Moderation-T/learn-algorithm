44935943cec05a330c765628607f92fb13c87508
yizhiyu <yizhiyu_web@163.com>
2020-05-19 21:23:55 +0800

    优化斐波那契递归

--- 递归/斐波那契数.js
+++ 递归/斐波那契数.js
@@ -4,8 +4,24 @@
 function fibonacciRecursion(n) {
   if (n === 0) return 0;
   if (n === 1) return 1;
-
   return fibonacciIteration(n - 1) + fibonacciIteration(n - 2);
+}
+
+// 递归方法优化 - 利用数组
+function fibonacciRecursionArray(n) {
+  const array = new Array(n); // 储存的是第 n 个数字的和
+  if (n === 0) {
+    return (array[0] = 0);
+  }
+  if (n === 1) {
+    return (array[1] = 1);
+  }
+
+  if (!array[n]) {
+    return (array[n] = fibonacciRecursionArray(n - 1) + fibonacciRecursionArray(n - 2));
+  }
+
+  return array[n];
 }
 
 // 迭代方法
@@ -24,5 +40,6 @@ function fibonacciIteration(n) {
   return second;
 }
 
-console.log('recursion', fibonacciRecursion(9));
-console.log('iteration', fibonacciIteration(9));
+console.log('recursion', fibonacciRecursion(23));
+console.log('iteration', fibonacciIteration(23));
+console.log('recursionArray', fibonacciRecursionArray(23));
